b21a220d64ed2590c09dced7b7da78454c2fca4b
Jennifer Taylor <dragonminded@dragonminded.com>
2022-09-24 17:55:57 +0000

    Add a few utilities to easily convert between cabinet and MAME nvram formats for testing.

---
+++ GridUtilities/cab2mame.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+import argparse
+from proto import SRAMProtocol
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Convert a cabinet SRAM dump to a format compatible for use in MAME.")
+    parser.add_argument(
+        'infile',
+        metavar="INFILE",
+        type=str,
+        help="File to load for conversion",
+    )
+    parser.add_argument(
+        'outfile',
+        metavar="OUTFILE",
+        type=str,
+        help="File to write after conversion",
+    )
+    args = parser.parse_args()
+    with open(args.infile, "rb") as ifp:
+        data = ifp.read()
+        if len(data) != 0x20000:
+            raise Exception("Input file is not the correct length!")
+
+        with open(args.outfile, "wb") as ofp:
+            ofp.write(b"".join(bytes([b, 0, 0, 0]) for b in data))
+
+    print(f"Wrote MAME-compatible SRAM file to {args.outfile}")
---
+++ GridUtilities/mame2cab.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python3
+import argparse
+from proto import SRAMProtocol
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Convert a MAME SRAM file to a format compatible for use on a cabinet.")
+    parser.add_argument(
+        'infile',
+        metavar="INFILE",
+        type=str,
+        help="File to load for conversion",
+    )
+    parser.add_argument(
+        'outfile',
+        metavar="OUTFILE",
+        type=str,
+        help="File to write after conversion",
+    )
+    args = parser.parse_args()
+    with open(args.infile, "rb") as ifp:
+        data = ifp.read()
+        if len(data) != 0x80000:
+            raise Exception("Input file is not the correct length!")
+
+        with open(args.outfile, "wb") as ofp:
+            ofp.write(data[::4])
+
+    print(f"Wrote cabinet-compatible SRAM file to {args.outfile}")
